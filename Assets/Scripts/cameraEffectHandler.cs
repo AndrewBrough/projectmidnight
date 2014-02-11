2aeb3c702cd830d2ef9a269d92f7212a06761a5a
Worldbrand <xrpkaldra@hotmail.com>
2014-02-11 14:59:55 -0500

    Added a bunch of scripts and stuff
    -cameraFader has been replaced by cameraEffectHandler
    - you should extend GameBehavior instead of MonoBehavior
    - added worldManager singleton; if you want to access something in the
    singleton, extend GameBehavior and use "world.player" (for example).

--- Assets/Scripts/cameraEffectHandler.cs
+++ Assets/Scripts/cameraEffectHandler.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class cameraFader : MonoBehaviour
-{
+public class cameraEffectHandler : GameBehaviour {
+	
 	public float lightLevel;
 	
     void Awake ()
@@ -13,4 +13,13 @@ public class cameraFader : MonoBehaviour
 	void Update(){
 		guiTexture.color = new Color(0,0,0,1-lightLevel);
 	}
+	
+	public void changeFOV(float degree){
+		world.camera.fov = degree;
+	}
+	
+	public void changeAngle(float angle){
+		world.camera.transform.Rotate(0, 0, angle);
+	}
+	
 }
