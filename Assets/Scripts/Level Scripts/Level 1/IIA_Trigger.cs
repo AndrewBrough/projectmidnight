1806f6d97e1a680c506fc7ef368883760bb21241
Eva Demers-Brett <evademersbrett@gmail.com>
2014-02-12 19:25:24 -0500

    Triggers up the 3c complete, issues with power cells

--- Assets/Scripts/Level Scripts/Level 1/IIA_Trigger.cs
+++ Assets/Scripts/Level Scripts/Level 1/IIA_Trigger.cs
@@ -6,28 +6,48 @@ using System.Collections;
 public class IIA_Trigger : MonoBehaviour {
 
 	public GameObject crate;
+	private GameObject player;
+	public GameObject monster;
 
 
-	bool triggered; 
+	bool crate_triggered; 
+	bool monster_triggered; 
 	private Vector3 targetPosition;
+
+
 
 	// Use this for initialization
 	void Start () {
 		targetPosition.z = crate.transform.position.z - 6;
-
+		player = GameObject.FindGameObjectWithTag ("Player");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (triggered) {
-			if(targetPosition.z < crate.transform.position.z)
-				crate.transform.Translate(0,0, -1, Space.World);
+		if (crate_triggered) {
+						if (targetPosition.z < crate.transform.position.z)
+								crate.transform.Translate (0, 0, -1, Space.World);
+				}
+		if (monster_triggered)
+				if (-22 > monster.transform.position.x)
+						monster.transform.Translate (Time.deltaTime *  8, 0, 0, Space.World);
+				else {
+						//this section is messy and I'm aware of it, basically, delete the instance of the monster
+						//then disable the trigger
+						UnityEngine.Object.DestroyObject (monster);
+						this.enabled = false;
 				}
 	}
 
-	void OnTriggerEnter() {
-		if (!triggered) 
-			triggered = true; 
+
+	void OnTriggerStay() {
+		if (!monster_triggered) {
+						if (Vector3.Angle (player.transform.forward, monster.transform.position - player.transform.position) < 40) {
+								//trigger monster
+								monster_triggered = true;
+								crate_triggered = true;
+						}
+				}
 	}
 }
---
+++ Assets/Scripts/Level Scripts/Level 1/IIIA_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IIIA_Trigger : MonoBehaviour {
+
+	public GameObject door;
+	public BoxCollider doorBoxCollider;
+
+	void OnTriggerEnter(){
+		//play open animation
+		door.animation.Play ("close");
+		//remove collider
+		doorBoxCollider.enabled = true;
+		//disable trigger
+		this.enabled = false;
+	}
+}
