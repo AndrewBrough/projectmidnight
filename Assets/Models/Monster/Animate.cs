b9600007665f0b89156d18ab3e8fa1dfd2322c58
Andrew Brough <spectrum1012@gmail.com>
2014-03-04 15:50:15 -0500

    nothing changed

---
+++ Assets/Models/Monster/Animate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animate : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		this.animator.Play("Walk");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
