3d2e970262b4f699095bfed9c5acedb6b1392f00
Eva Demers-Brett <evademersbrett@gmail.com>
2014-03-09 19:36:54 -0400

    Sound effects up to hallway sequence completed.

---
+++ Assets/Scripts/Level Scripts/Level 1/IIC_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IIC_Trigger : MonoBehaviour {
+
+	public GameObject sound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter() {
+		sound.audio.Play ();
+
+	}
+}
